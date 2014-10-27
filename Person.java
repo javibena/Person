16c885b461d85e20e41f2f2aa8a8047ed61d2038
Javi <jbenavideseco@gmail.com>
2014-10-27 09:41:01 +0100

    creada la clase person, pide y devuelve la edad

---
+++ Person.java
@@ -0,0 +1,27 @@
+
+/**
+ * Write a description of class Person here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Person
+{
+    // instance variables - replace the example below with your own
+    private int age;
+
+    /**
+     * Constructor for objects of class Person
+     */
+    public Person(int newAge)
+    {
+     age = newAge;
+    }
+    /**
+     * Devuelve la edad de la persona.
+     */
+    public int getAge()
+    {
+        return age;
+    }
+}
